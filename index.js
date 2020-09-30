fc795a79ce1791963fd2604e0fb2ac66ad1fc353
Pathum <pathummadushanka.weerathunga@student.kyh.se>
2020-09-30 23:21:00 +0200

    Last version 2020_09_30

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
+let value1;
+let value2;
+let txtLog;
+let operator;
+let result;
 
-function myFunction() {
-    alert("Helllo world from external file");
-    console.log("my function");
+
+function onClickBtn(value){
+        txtLog = document.getElementById("textBox").value;
+        document.getElementById("textBox").value = txtLog+value;
 }
 
-let a = [5,10,4,7];
+function onClickOperator(operatorValue) {
+    operator=operatorValue;
+    value1 = parseFloat(document.getElementById("textBox").value);
+    document.getElementById("textBox").value='';
 
-console.log([2]);
-console.log("Hello");
+}
+
+function onClickEquel(){
+
+    if(!isNaN(parseFloat(document.getElementById("textBox").value))){
+        value2 = parseFloat(document.getElementById("textBox").value);
+        document.getElementById("textBox").value = '';
+    }
+    if(!isNaN(value1) && !isNaN(value2)){
+        doMath();
+    }
+    
+}
+
+function doMath(){
+
+    switch(operator){
+        case "/":
+            result = value1 / value2;
+            break;
+        case "x":
+            result = value1 * value2;
+            break;
+        case "-":
+            result = value1 - value2;
+            break;
+        case "+":
+            result = value1 + value2;
+            break;
+        default:
+            console.log("Error");
+    }
+
+    document.getElementById("textBox").value = result;
+    
+}
+
+function onClickClear() {
+    document.getElementById("textBox").value = '';
+}
+
+
